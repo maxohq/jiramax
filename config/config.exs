756b22a27cc296a13673e2cd1a8efa7a5731d01a
Roman Heinrich <roman.heinrich@gmail.com>
2023-11-06 16:49:47 +0100

    chore: regenerate with separate folder for schemas / operations

--- config/config.exs
+++ config/config.exs
@@ -4,6 +4,9 @@ config :oapi_generator,
   default: [
     output: [
       base_module: Jiramax,
+      default_operation_module: Ops,
+      operation_subdirectory: "operations/",
+      schema_subdirectory: "schemas/",
       location: "lib/jiramax"
     ]
   ]
--- lib/jiramax.ex
+++ lib/jiramax.ex
@@ -2,17 +2,4 @@ defmodule Jiramax do
   @moduledoc """
   Documentation for `Jiramax`.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Jiramax.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
